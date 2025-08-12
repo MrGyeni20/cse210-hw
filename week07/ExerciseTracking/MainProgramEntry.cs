e3665bbbc62236d0740242e7c84eec27d64f7df9
MrGyeni20 <ngyeni@byupathway.edu>
2025-08-12 04:07:16 +0000

    week07

---
+++ week07/ExerciseTracking/MainProgramEntry.cs
@@ -0,0 +1,17 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 3.0),
+            new Cycling(new DateTime(2022, 11, 3), 30, 9.7),
+            new Swimming(new DateTime(2022, 11, 3), 30, 20)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,87 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+// Base class
+public abstract class Activity
 {
-    static void Main(string[] args)
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public int GetMinutes() => _minutes;
+    public DateTime GetDate() => _date;
+
+    public abstract double GetDistance(); // in miles
+    public abstract double GetSpeed();    // in mph
+    public abstract double GetPace();     // in min per mile
+
+    public virtual string GetSummary()
+    {
+        return $"{_date.ToString("dd MMM yyyy")} {this.GetType().Name} ({_minutes} min): " +
+               $"Distance {GetDistance():0.0} miles, Speed {GetSpeed():0.0} mph, Pace: {GetPace():0.0} min per mile";
+    }
+}
+
+// Running class
+public class Running : Activity
+{
+    private double _distance; // in miles
+
+    public Running(DateTime date, int minutes, double distance)
+        : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance() => _distance;
+    public override double GetSpeed() => (_distance / GetMinutes()) * 60;
+    public override double GetPace() => GetMinutes() / _distance;
+}
+
+// Cycling class
+public class Cycling : Activity
+{
+    private double _speed; // in mph
+
+    public Cycling(DateTime date, int minutes, double speed)
+        : base(date, minutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetSpeed() => _speed;
+    public override double GetDistance() => (_speed * GetMinutes()) / 60;
+    public override double GetPace() => 60 / _speed;
+}
+
+// Swimming class
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(DateTime date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return _laps * 50 / 1000.0 * 0.62; // Convert meters to miles
+    }
+
+    public override double GetSpeed()
+    {
+        return GetDistance() / GetMinutes() * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
     }
 }
